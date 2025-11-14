5e13cedd1fd6bbb6e171994d15b4ba51b8c03165
atillatrisakti <atillasilvaaa@gmail.com>
2025-11-14 14:54:20 +0700

    feat: login

---
+++ src/validations/auth-validation.ts
@@ -0,0 +1,8 @@
+import z from "zod";
+
+export const loginSchemaForm = z.object({
+  email: z.string().min(1, "Email is required").email("Invalid email address"),
+  password: z.string().min(1, "Password is required"),
+});
+
+export type LoginForm = z.infer<typeof loginSchemaForm>;
