67178635d430a16c6068914c7232ee399dfd5dfd
atillatrisakti <atillasilvaaa@gmail.com>
2026-01-08 14:41:23 +0700

    feat: create user

---
+++ src/app/(dashboard)/admin/user/_components/dialog-create-user.tsx
@@ -0,0 +1,115 @@
+import FormInput from "@/components/common/form-input";
+import { Button } from "@/components/ui/button";
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Form } from "@/components/ui/form";
+import {
+  INITIAL_CREATE_USER_FORM,
+  INITIAL_STATE_CREATE_USER,
+  USER_ROLES,
+} from "@/constants/auth-constant";
+import {
+  CreateUserForm,
+  createUserSchemaForm,
+} from "@/validations/auth-validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { startTransition, useActionState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { createUser } from "../actions";
+import { toast } from "sonner";
+import FormSelect from "@/components/common/form-select";
+
+export default function DialogCreateUser({ refetch }: { refetch: () => void }) {
+  const form = useForm<CreateUserForm>({
+    resolver: zodResolver(createUserSchemaForm),
+    defaultValues: INITIAL_CREATE_USER_FORM,
+  });
+
+  const [createUserState, createUserAction, isPendingCreateUser] =
+    useActionState(createUser, INITIAL_STATE_CREATE_USER);
+
+  const onSubmit = form.handleSubmit(async (data) => {
+    const formData = new FormData();
+    Object.entries(data).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+    startTransition(() => {
+      createUserAction(formData);
+    });
+  });
+
+  useEffect(() => {
+    if (createUserState?.status === "error") {
+      toast.error("Create User Failed", {
+        description: createUserState.errors?._form?.[0],
+      });
+    }
+
+    if (createUserState?.status === "success") {
+      toast.success("User Created Successfully");
+      form.reset();
+      document.querySelector<HTMLButtonElement>('[data-state="open"]')?.click();
+      refetch();
+    }
+  }, [createUserState]);
+  return (
+    <DialogContent className="sm:max-w-[425px]">
+      <Form {...form}>
+        <DialogHeader>
+          <DialogTitle>Create User</DialogTitle>
+          <DialogDescription>
+            Fill the form below to create a new user account.
+          </DialogDescription>
+        </DialogHeader>
+        <form onSubmit={onSubmit} className="space-y-4">
+          <FormInput
+            form={form}
+            name="name"
+            label="Name"
+            placeholder="Insert your name"
+          />
+          <FormInput
+            form={form}
+            name="email"
+            label="Email"
+            placeholder="Insert your email"
+            type="email"
+          />
+
+          <FormSelect
+            form={form}
+            name="role"
+            label="Role"
+            selectItems={USER_ROLES}
+          />
+          <FormInput
+            form={form}
+            name="password"
+            label="Password"
+            placeholder="********"
+            type="password"
+          />
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">
+              {isPendingCreateUser ? (
+                <Loader2 className="animate-spin" />
+              ) : (
+                "Create"
+              )}
+            </Button>
+          </DialogFooter>
+        </form>
+      </Form>
+    </DialogContent>
+  );
+}
--- src/validations/auth-validation.ts
+++ src/validations/auth-validation.ts
@@ -5,4 +5,16 @@ export const loginSchemaForm = z.object({
   password: z.string().min(1, "Password is required"),
 });
 
+export const createUserSchemaForm = z.object({
+  email: z.string().min(1, "Email is required").email("Invalid email address"),
+  password: z.string().min(1, "Password is required"),
+  name: z.string().min(1, "Name is required"),
+  role: z.string().min(1, "Role is required"),
+  // avatar_url: z.union([
+  //   z.string().min(1, "Avatar URL is required"),
+  //   z.instanceof(File),
+  // ]),
+});
+
 export type LoginForm = z.infer<typeof loginSchemaForm>;
+export type CreateUserForm = z.infer<typeof createUserSchemaForm>;
